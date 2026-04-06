922e3684bf287ca7097be9057206dc11a12a5cba
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-04-06 12:23:22 +0530

    Add SVG icons for Bluesky, Discord, Documentation, GitHub, Social, and X

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookie from "cookie-parser";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // Importing routes
 import authRouter from "./routes/authRoutes.js";
@@ -9,10 +11,18 @@ import gameRouter from "./routes/game.routes.js";
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(cookie()); // Middleware to parse cookies
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-negotiation-bnrw.onrender.com",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -22,6 +32,10 @@ app.use(
 //call the Routes
 app.use("/api/auth", authRouter);
 app.use("/api/games", gameRouter);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
 //export default app
 export default app;
